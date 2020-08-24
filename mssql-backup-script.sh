fcbd1cb84f2224477df66f3460b9cbc0a1a05406
Ashley Cawley <ash@ashleycawley.co.uk>
2020-08-24 12:10:09 +0100

    Switched sqlcmd to use full binary path which works better with cron

--- mssql-backup-script.sh
+++ mssql-backup-script.sh
@@ -25,7 +25,7 @@ TIMESTAMP=$(date +"%F")
 mkdir -p $BACKUP_DIR
 
 # Performs the MS SQL database backup
-sqlcmd -S localhost -Q "BACKUP DATABASE SOS_Datatables TO DISK = N'$BACKUP_DIR/SOS_Datatables-$TIMESTAMP.bak' WITH NOFORMAT, NOINIT, SKIP, NOREWIND, STATS=10" -U $SA_USER -P $SA_PASS
+/opt/mssql-tools/bin/sqlcmd -S localhost -Q "BACKUP DATABASE SOS_Datatables TO DISK = N'$BACKUP_DIR/SOS_Datatables-$TIMESTAMP.bak' WITH NOFORMAT, NOINIT, SKIP, NOREWIND, STATS=10" -U $SA_USER -P $SA_PASS
 
 # Removes backups that are older than 7 days
 find /var/opt/mssql/backup/data/ -mindepth 1 -mtime +$NUMBER_OF_DAYS_OF_BACKUPS_TO_RETAIN -delete 
